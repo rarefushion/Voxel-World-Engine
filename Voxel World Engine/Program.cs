88220c061e46c2d32c4bc464d7d92505f929cfc3
rarefushion <rarefushion@gmail.com>
2023-03-03 05:53:58 -0800

    Initial Files

---
+++ Voxel World Engine/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Windowing.Desktop;
+
+
+namespace Voxel_World_Engine
+{
+
+    internal class Program
+    {
+        private static TestWindow window = new TestWindow(GameWindowSettings.Default, NativeWindowSettings.Default);
+
+        static void Main(string[] args)
+        {
+            window.Run();
+        }
+
+    }
+}
---
+++ Voxel World Engine/TestWindow.cs
@@ -0,0 +1,134 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+
+namespace Voxel_World_Engine
+{
+    internal class TestWindow : GameWindow
+    {
+        private int vertexBufferHandle;
+        private int shaderProgramHandle;
+        private int vertexArrayHandle;
+
+        public TestWindow(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
+        {
+            this.CenterWindow(new Vector2i(1920, 1080));
+        }
+
+        protected override void OnLoad()
+        {
+            GL.ClearColor(new Color4(0f, 0f, 0f, 1f));
+
+            float[] vertices = new float[]
+            {
+                0f, 0.5f, 0f,
+                0.5f, -0.5f, 0f,
+                -0.5f, -0.5f, 0f
+            };
+
+            vertexBufferHandle = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBufferHandle);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+
+            vertexArrayHandle = GL.GenVertexArray();
+            GL.BindVertexArray(vertexArrayHandle);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBufferHandle);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            GL.BindVertexArray(0);
+
+            string vertexShaderCode = 
+                @"
+                #version 330 core
+
+                layout (location = 0) in vec3 aPosition;
+
+                void main()
+                {
+                    gl_Position = vec4(aPosition, 1f);
+                }
+                ";
+
+            string fragmentShaderCode =
+                @"
+                #version 330 core
+
+                out vec4 result;
+
+                void main()
+                {
+                    result = vec4(0f, 0.8f, 1f, 1f);
+                }
+                ";
+
+            int vertexShaderHandle = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShaderHandle, vertexShaderCode);
+            GL.CompileShader(vertexShaderHandle);
+
+            int fragmentShaderHandle = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentShaderHandle, fragmentShaderCode);
+            GL.CompileShader(fragmentShaderHandle);
+
+            shaderProgramHandle = GL.CreateProgram();
+
+            GL.AttachShader(shaderProgramHandle, vertexShaderHandle);
+            GL.AttachShader(shaderProgramHandle, fragmentShaderHandle);
+
+            GL.LinkProgram(shaderProgramHandle);
+
+            GL.DetachShader(shaderProgramHandle, vertexShaderHandle);
+            GL.DetachShader(shaderProgramHandle, fragmentShaderHandle);
+
+            GL.DeleteShader(vertexShaderHandle);
+            GL.DeleteShader(fragmentShaderHandle);
+
+            base.OnLoad();
+        }
+
+        protected override void OnResize (ResizeEventArgs e)
+        {
+            GL.Viewport(0, 0, e.Width, e.Height);
+            base.OnResize(e);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.UseProgram(shaderProgramHandle);
+            GL.BindVertexArray(vertexArrayHandle);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+            this.Context.SwapBuffers();
+            base.OnRenderFrame(args);
+        }
+
+        protected override void OnUnload()
+        {
+            GL.BindVertexArray(0);
+            GL.DeleteVertexArray(vertexArrayHandle);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.DeleteBuffer(vertexBufferHandle);
+
+            GL.UseProgram(0);
+            GL.DeleteProgram(shaderProgramHandle);
+
+            base.OnUnload();
+        }
+    }
+}
